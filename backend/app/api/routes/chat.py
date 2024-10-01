82fc3439c5cc0123d0be3e899a4ae09e69d74aad
jasc <jan.schnegotzki@zuhlke.com>
2024-10-01 16:28:59 +0800

    Add endpoint for chat and call OpenAI to generate output

--- backend/app/api/routes/chat.py
+++ backend/app/api/routes/chat.py
@@ -1,10 +1,26 @@
-from typing import Any
-
 from fastapi import APIRouter
+from openai import OpenAI
+from pydantic import BaseModel
 
 router = APIRouter()
 
+class Prompt(BaseModel):
+    prompt: str
 
-@router.get("/chat", response_model=str)
-def chat() -> Any:
-    return "Working!"
+class ChatResponse(BaseModel):
+    response: str
+
+@router.post("/chat", response_model=ChatResponse)
+def chat(prompt: Prompt) -> ChatResponse:
+    client = OpenAI()
+
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt.prompt
+            }
+        ]
+    )
+    return {"response": completion.choices[0].message.content}
--- backend/app/tests/api/routes/test_chat.py
+++ backend/app/tests/api/routes/test_chat.py
@@ -1,13 +1,20 @@
 from app.core.config import settings
+from app.api.routes.chat import chat
 from fastapi.testclient import TestClient
 
 
-def test_get_users_superuser_me(
-    client: TestClient, superuser_token_headers: dict[str, str]
+def test_when_called_with_incorrect_body_format_returns_error(
+    client: TestClient
 ) -> None:
-    r = client.get(f"{settings.API_V1_STR}/users/me", headers=superuser_token_headers)
-    current_user = r.json()
-    assert current_user
-    assert current_user["is_active"] is True
-    assert current_user["is_superuser"]
-    assert current_user["email"] == settings.FIRST_SUPERUSER
+    r = client.post(f"{settings.API_V1_STR}/chat", json={"this": "is wrong"})
+    content = r.json()
+    assert content
+    assert r.status_code == 422
+
+def test_when_called_forwards_to_llm(
+        client: TestClient
+) -> None:
+    r = client.post(f"{settings.API_V1_STR}/chat", json={"prompt": "Hello"})
+    content = r.json()
+    assert content['response']
+    assert r.status_code == 200
